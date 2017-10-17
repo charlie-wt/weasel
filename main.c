fdae8e6b970a4694741fc45b7ee9d08eced2e9db
charlie-wt <cwt1g14@soton.ac.uk>
2017-10-17 21:20:03 +0100

    cleanup.

--- main.c
+++ main.c
@@ -1,33 +1,24 @@
 #include "main.h"
 
 int main () {
-	// our alphabet of characters to choose from.
-	char characters[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ ";
-	int numchars = (int)strlen(characters);
-
-	// the phrase to get.
+	// initialise
+	char* characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ ";
 	char* target = "Methinks it is like a weasel";
+
+	int pop_size = 10;
+	int numchars = (int)strlen(characters);
 	int length = (int)strlen(target);
 
-//	// the string to evolve.
-//	char current[length+1];
-//	for ( int i=0; i < length; i++ ) {
-//		int idx = (int)floor(rand() % numchars);
-//		current[i] = characters[idx];
-//	}
-//	current[length] = '\0';
-//
-//	printf("   Target string: %s\n", target);
-//	printf("Generated string: %s\n", current);
-
-	printf("--- testing gen_pop ---\n");
-	int pop_size = 10;
 	char pop[pop_size][length+1];
 	gen_population(numchars, characters, pop_size, length, pop);
+
+	// print
+	printf("  target: %s\n", target);
 	for ( int i=0; i<pop_size; i++ ) {
 		printf("member %i: %s\n", i, pop[i]);
 	}
 
+	// return
 	return 0;
 }
 
@@ -36,12 +27,13 @@ void gen_population ( int numchars, char characters[numchars+1],
                       int size, int len_target, char pop[size][len_target+1] ) {
 	for ( int j=0; j < size; j++ ) {
 		char current[len_target+1];
+
 		for ( int i=0; i < len_target; i++ ) {
 			int idx = (int)floor(rand() % numchars);
 			current[i] = characters[idx];
 		}
 		current[len_target] = '\0';
-//		pop[j] = current;
+
 		strcpy(pop[j], current);
 	}
 }
